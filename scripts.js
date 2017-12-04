34517a1982fa191b14608eb5385cdffd756e6320
LukaszWiszniewski <wisznu07@gmail.com>
2017-12-04 21:20:33 +0100

    Added page and script files

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var container = document.getElementById('container'),
+    newButton =  document.createElement('button');
+
+function addButton(numberOfButtons){
+for(var i=0; i<5;i++){
+newButton.innerHTML = 'Alert'+number;
+}
+}
