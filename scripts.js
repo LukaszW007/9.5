74be1bcdd856624bb516a4917f06e31d5e09cf68
LukaszWiszniewski <wisznu07@gmail.com>
2017-12-05 12:42:13 +0100

    Added script to buttons work

--- scripts.js
+++ scripts.js
@@ -1,8 +1,21 @@
-var container = document.getElementById('container'),
-    newButton =  document.createElement('button');
+var listOfButtons = document.getElementsByClassName('button'),
+    newButton,
+    nuberOfButtons,
+    textInButton;
 
-function addButton(numberOfButtons){
-for(var i=0; i<5;i++){
-newButton.innerHTML = 'Alert'+number;
+function addButtons(numberOfButtons) {
+    for (var i = 0; i < numberOfButtons; i++) {
+        newButton = document.createElement('BUTTON');
+        newButton.className = 'button';
+        newButton.innerHTML = 'Alert' + (i+1);
+        document.body.appendChild(document.createElement('br'));
+        document.body.appendChild(newButton);
+    }
 }
+function alertReader(){
+    listOfButtons = document.getElementsByClassName('button');
+    for (var j=0;j<listOfButtons.length;j++){
+        textInButton = listOfButtons[j].innerText;
+        alert(textInButton);
+    }
 }
